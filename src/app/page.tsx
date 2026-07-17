94d3fd1a55a1f0518a7de89140c5416038e3c258
Richard10454 <richard2512006@gmail.com>
2026-07-17 15:15:21 -0400

    feat: eliminar componente Services y su referencia en Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import HeroImage from "@/components/HeroImage";
-import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <>
       <HeroImage />
-      <Services />
     </>
   );
 }
